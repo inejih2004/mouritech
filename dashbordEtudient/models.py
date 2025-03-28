317be55dc622510132ec7be35a3aaeeebe7d17b2
cheikh Hama <cheikhhama20@gmail.com>
2025-03-28 12:27:55 +0000

    create dashbord etudien

--- dashbordEtudient/models.py
+++ dashbordEtudient/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('admin', 'Admin'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
+    def _str_(self):
+        return self.user.username
